3ccdf795626294a4f6c1823b489ee7e72d25debf
allgaeulab <edv@allgaeulab.de>
2011-03-31 07:59:32 +0200

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Weborder1::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weborder1::Application.config.secret_token = '1e461254404103ff26e20f7d032ba44e86cb1853bf96b8c5770191ec97f365399b3a725e2051d80a71c37520a879001a9dc9a40c62e996d2d93ffd2f871b7c89'
